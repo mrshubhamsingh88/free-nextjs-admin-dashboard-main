fef1abd88f8eb12884ce2ea0a6ec1592d1f513a5
Shubham SIngh <shubham@proeffico.com>
2025-03-22 18:27:43 +0530

    [NEW]

--- src/app/(admin)/(others-pages)/blankNew/page.tsx
+++ src/app/(admin)/(others-pages)/blankNew/page.tsx
@@ -5,7 +5,7 @@ import React from "react";
 export const metadata: Metadata = {
   title:
     "Best Friends Forever: Panda & Princess Edition 🌸🐼👑",
-  description: "You’re the princess of my world, and together, we create a story as magical as a fairy tale. With you by my side, every moment feels like a dream come true. 👑✨",
+  description: "You&apos;re the princess of my world, and together, we create a story as magical as a fairy tale. With you by my side, every moment feels like a dream come true. 👑✨",
 };
 
 export default function BlankPage() {
@@ -20,23 +20,23 @@ export default function BlankPage() {
           <p className="text-sm text-gray-500 dark:text-gray-400 sm:text-base">
             Im really 🥺👉👈Sorry
             <br /><br />mani apko hurt kiya 😞 apko preshan kiya 😣 apki tabit khrab the fir bhi i hurt youu 😔 im really for apka supper admin bhot ganda 😡 apko hurt kr the hai apsy fight kr the hai 😢
-            always apko hurt kr the hai 😓 .... this time apko uncomfortable feel bhi kra diya im worry 😟 sorry bachha 😞 but trust me mai kabhi bhi nhi socha skthe apko uncomfortable feel krna apko hurt krna 💔
-            i know mai apko bhot preshan kr the hu 🥺 bhot fight bhi kr the hu 🥲 ...<br /><br />
-            but the truth was mai apki bina ek din bhi nhi rha pata 😞 agar tum nhi mil the i feel like i dont want anything more...<br /><br />
-            my day incomplete without you 💖 without my Beautiful panda🐼 your the reason of my happiness 😊 my smile 😁 your the reason of i come child front of you 💕....<br /><br />
+            always apko hurt kr the hai 😓 this time apko uncomfortable feel bhi kra diya im worry 😟 sorry bachha 😞 but trust me mai kabhi bhi nhi socha skthe apko uncomfortable feel krna apko hurt krna 💔
+            i know mai apko bhot preshan kr the hu 🥺 bhot fight bhi kr the hu 🥲<br /><br />
+            but the truth was mai apki bina ek din bhi nhi rha pata 😞 agar tum nhi mil the i feel like i dont want anything more<br /><br />
+            my day incomplete without you 💖 without my Beautiful panda🐼 your the reason of my happiness 😊 my smile 😁 your the reason of i come child front of you 💕<br /><br />
 
-            i know your also scared to lose me 😱 or mujhe jana hai ahh your always with me na..??? ahhhh what you feel for me tum rh late ho kya mare bina.... i want to know...🤔<br /><br />
+            i know your also scared to lose me 😱 or mujhe jana hai ahh your always with me na??? ahhhh what you feel for me tum rh late ho kya mare bina i want to know🤔<br /><br />
 
-            i know mani os din apko hurt kiya the 😔 ajjj mai fir sy scratch sy sab krni jara hu 💪 fir sy apki apni khaani likh ni jara hu ✍.... i hope is bar bas khushiyan hi likho 😊🌸 (i know we
-            are fight too much sometimes 😅 but the fight also makes us both closer ❤) after complete.. i want my dreams to come true 🌟.... and get the same place again in panda's deep place 🐼💖...<br /><br />
+            i know mani os din apko hurt kiya the 😔 ajjj mai fir sy scratch sy sab krni jara hu 💪 fir sy apki apni khaani likh ni jara hu ✍ i hope is bar bas khushiyan hi likho 😊🌸 (i know we
+            are fight too much sometimes 😅 but the fight also makes us both closer ❤) after complete.. i want my dreams to come true 🌟 and get the same place again in panda&apos;s deep place 🐼💖<br /><br />
 
             Sorry panda 🐼💖
             i hope your happy to see this all 😇 i want to show you how much your special for me 🥰 ase nhi bas mai apko apna favorite person bol the hu 🏆 ahhh yes your my favorite person 💖 my favorite girl 💕
-            ahh i feel happy 😊 comfort 🛋 safe 🛡 when you around me 🤗<br /><br />i want you... mai chathe tum mare pass rhoo 🥺 (Madam ji madam ji vitamin panda miliga kya?)....
+            ahh i feel happy 😊 comfort 🛋 safe 🛡 when you around me 🤗<br /><br />i want you mai chathe tum mare pass rhoo 🥺 (Madam ji madam ji vitamin panda miliga kya?)
           </p>
           <br /><br />
           <p className="text-sm text-gray-500 dark:text-gray-400 sm:text-base">
-            Waiting for reply... ⏳💌
+            Waiting for reply ⏳💌
           </p>
 
         </div>
