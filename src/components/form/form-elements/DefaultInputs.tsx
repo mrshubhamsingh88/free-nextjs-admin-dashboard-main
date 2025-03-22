9066a4bb919d06770be6702e87ff9ed101c19c02
Shubham SIngh <shubham@proeffico.com>
2025-03-22 19:07:38 +0530

    [nEW]

--- src/components/form/form-elements/DefaultInputs.tsx
+++ src/components/form/form-elements/DefaultInputs.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import ComponentCard from "../../common/ComponentCard";
 import Label from "../Label";
 import Input from "../input/InputField";
